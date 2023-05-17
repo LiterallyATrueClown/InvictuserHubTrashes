f0f6c36c5361854ee4bebcb6ec6319506eb51862
HecatiA <88549714+LiterallyATrueClown@users.noreply.github.com>
2023-05-18 00:20:27 +0700

    Update USBossesQuests.lua

--- USBossesQuests.lua
+++ USBossesQuests.lua
@@ -6,7 +6,7 @@ return {
   {"Muffet tel",30,40},
   {"Mettaton EX tel",40,60},
   {"Asgore tel",60,70},
-  {"Asriel tel",90,100},
+  {"Asriel tel",70,100},
   {"Undying Undyne tel",100,110},
   {"Sans tel",110,120},
   {"Chara tel",120,140},

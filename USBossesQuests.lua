0bf22948341031d8909c164f107c55becb361592
HecatiA <88549714+LiterallyATrueClown@users.noreply.github.com>
2023-05-17 23:58:21 +0700

    Update USBossesQuests.lua

--- USBossesQuests.lua
+++ USBossesQuests.lua
@@ -1,3 +1,14 @@
 return {
-{"Flowey tel",1,2},
+  {"Flowey tel",1,2},
+  {"Toriel tel",2,6},
+  {"Papyrus tel",6,15},
+  {"Undyne tel",15,30},
+  {"Muffet tel",30,40},
+  {"Mettaton EX tel",40,60},
+  {"Asgore tel",60,70},
+  {"Asriel tel",90,100},
+  {"Undying Undyne tel",100,110},
+  {"Sans tel",110,120},
+  {"Chara tel",120,140},
+  {"Omega flowey tel",140,160},
 };
